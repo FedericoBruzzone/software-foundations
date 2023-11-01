6f4c1fa2d638284ee092ea2e72e4c0c53873c041
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-11-01 12:27:07 +0100

    Update exw4.v
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- vol-1/assignements/4/exw4.v
+++ vol-1/assignements/4/exw4.v
@@ -8,7 +8,7 @@ From Coq Require Import Arith.EqNat. Import Nat.
 You will need a lemma first. *)
 
 Inductive beautiful : nat -> Prop :=
-|  b_0   : beautiful 0
+| b_0   : beautiful 0
 | b_3   : beautiful 3
 | b_5   : beautiful 5
 | b_sum : forall n m, beautiful n -> beautiful m -> beautiful (n+m).
@@ -18,8 +18,24 @@ Inductive gorgeous : nat -> Prop :=
 | g_plus3 : forall n, gorgeous n -> gorgeous (3+n)
 | g_plus5 : forall n, gorgeous n -> gorgeous (5+n).
 
+Lemma g_sum : forall n m, gorgeous n -> gorgeous m -> gorgeous (n+m).
+Proof.
+  intros n m Hn Hm.
+  induction Hn as [| | n' Hn' IHn'].
+  - simpl. apply Hm.
+  - simpl. apply g_plus3. apply IHHn.
+  - simpl. apply g_plus5. apply IHn'.
+Qed.
+
 Theorem beautiful_gorgeous : forall n, beautiful n -> gorgeous n.
-  Admitted.
+Proof.
+  intros n H.
+   induction H as [| | | n m Hn Hm IHn IHm].
+  - apply g_0.
+  - apply g_plus3. apply g_0.
+  - apply g_plus5. apply g_0.
+  - apply g_sum. apply Hm. apply IHm.
+Qed.
 
 (** Write a txt file with the mathematical proof: I'll get you started with
 the more involved case:
@@ -44,21 +60,38 @@ Complete the other cases specifying which rules you are applying as above
 of them are not completely trivial and require previous lemmas, so think before you start writing tactics*)
 
 Lemma le_trans : forall m n o, m <= n -> n <= o -> m <= o.
-Admitted.
-
+Proof.
+  (* Check le_S. *)
+  (* Locate le_S. *)
+  (* Print Coq.Init.Peano. *)
+  intros m n o H1 H2.
+  induction H2 as [| o' H2' IH].
+  - apply H1.
+  - apply le_S. apply IH.
+Qed.
 
 Theorem O_le_n : forall n,
     0 <= n.
-Admitted.
+Proof.
+  intros n.
+  induction n as [| n' IH].
+  - reflexivity.
+  - apply le_S. apply IH.
+Qed.
 
 Theorem n_le_m__Sn_le_Sm : forall n m,
     n <= m -> S n <= S m.
-Admitted.
-
+Proof.
+  intros n m H.
+  apply le_n_S. apply H.
+Qed.
 
 Theorem Sn_le_Sm__n_le_m : forall n m,
     S n <= S m -> n <= m.
-Admitted.
+Proof.
+  intros n m H.
+  apply le_S_n. apply H.
+Qed.
 
 (** Most of the results above are needed in the following, where I'll start the proof, since
 it requires the IH to be general enough.  Remember that < is defined in terms of <=
