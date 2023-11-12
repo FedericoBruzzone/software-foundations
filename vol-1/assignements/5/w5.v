0e1f1d114fbaa2cb62949cf042c5d54abd569610
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-11-12 20:43:54 +0100

    Add 5 and repeat
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

---
+++ vol-1/assignements/5/w5.v
@@ -0,0 +1,73 @@
+
+From Coq Require Import Bool.Bool.
+From Coq Require Import Init.Nat.
+From Coq Require Import Arith.Arith.
+From Coq Require Import Arith.EqNat.
+
+(*  Remember the definition of arithmetic and Boolean expression and their interpreters:*)
+
+Inductive aexp : Type :=
+  | ANum : nat -> aexp
+  | APlus : aexp -> aexp -> aexp
+  | AMinus : aexp -> aexp -> aexp
+  | AMult : aexp -> aexp -> aexp.
+
+Inductive bexp : Type :=
+  | BTrue : bexp
+  | BFalse : bexp
+  | BEq : aexp -> aexp -> bexp
+  | BLe : aexp -> aexp -> bexp
+  | BNot : bexp -> bexp
+  | BAnd : bexp -> bexp -> bexp.
+
+Fixpoint aeval (a : aexp) : nat :=
+  match a with
+  | ANum n => n
+  | APlus a1 a2 => (aeval a1) + (aeval a2)
+  | AMinus a1 a2  => (aeval a1) - (aeval a2)
+  | AMult a1 a2 => (aeval a1) * (aeval a2)
+  end
+.
+Fixpoint beval (b : bexp) : bool :=
+  match b with
+  | BTrue       => true
+  | BFalse      => false
+   | BEq a1 a2   => (aeval a1) =? (aeval a2)
+  | BLe a1 a2   => (aeval a1) <=? (aeval a2)
+  | BNot b1     => negb (beval b1)
+  | BAnd b1 b2  => andb (beval b1) (beval b2)
+  end.
+
+(* This is the optimizer for arith expressions *)
+
+
+Fixpoint optimize_0plus (a:aexp) : aexp :=
+  match a with
+  | ANum n =>
+      ANum n
+  | APlus (ANum 0) e2 =>
+      optimize_0plus e2  (* <-- here ! *)
+  | APlus e1 e2 =>
+      APlus (optimize_0plus e1) (optimize_0plus e2)
+  | AMinus e1 e2 =>
+      AMinus (optimize_0plus e1) (optimize_0plus e2)
+  | AMult e1 e2 =>
+      AMult (optimize_0plus e1) (optimize_0plus e2)
+  end.
+
+(* 1 Define a similar optimization on Boolean expression:
+
+Fixpoint opt_b (b : bexp) : bexp :=
+...
+
+such that 
+
+  - on arithmetic sub-expressions, it applies the above optimization optimize_0plus
+  - further, it performs the following optimization:
+
+        - (true and b) is optimized to to b
+        - (false and b) is optimized  to false *)
+
+
+(* 2 Prove that the transformation over Booleans is sound. Use the tacticals we've seen so
+    far to make the proof as short and modular as possible. *)
---
+++ vol-1/assignements/repeat.v
@@ -0,0 +1,73 @@
+
+Set Warnings "-notation-overridden,-parsing".
+From Coq Require Import Bool.Bool.
+From Coq Require Import Init.Nat.
+From Coq Require Import Arith.Arith.
+From Coq Require Import Arith.EqNat.
+From Coq Require Import Lia.
+From Coq Require Import Lists.List.
+From Coq Require Import Strings.String.
+Import ListNotations.
+
+From LF Require Import Imp.
+
+
+(* 
+
+Let's go back to the IMP language described in Imp.v
+
+Consider the REPEAT construct, whose operational semantics is
+described by the following rules
+
+
+  st =[ c ]=>  st'       beval st' b = true
+------------------------------------------------ (E_Re_End)
+    st =[ REPEAT c UNTIL b END ] => st'  
+
+
+   st =[ c ]=>  st'   beval st' b = false  st' =[ REPEAT c UNTIL b END ] => st''   
+--------------------------------------------------------------------------------- (E_Re_Loop)
+                st =[ REPEAT c UNTIL b END ] => st''  
+
+
+In fact, REPEAT does *not* need to be taken as primitive, but it can be
+*defined* in terms of the constructors of com.
+
+1.1. ***Define*** it in terms of the original language of com, using the 
+While construct. Fill in the following definition:
+ *)
+
+ Module RepeatDefined.
+ 
+   
+ Definition REP  (c : com) (b : bexp) : com.				     
+(* your definition here *)
+ 
+ (*
+ 
+ 1.2. Introduce an appropriate Notation.
+ 
+ 1.3. Prove the above rules E_Re_End and E_Re_Loop are *derived*
+ rules. That is, they do not need to be defined inductively but just
+ stated and proven as Theorems, with respect to the original ceval
+ relation.
+
+ Again, do NOT redefine ceval.
+  
+ 1.4 State and prove the inversion principle for the construct, that
+ is a Theorem like:
+ 
+ forall b c st st', st =[(REPEAT c UNTIL b END) => st' implies that
+ either b is true in st and st=[ c] => st' or there exists a st'' such
+ that ....
+ 
+ This theorem makes explicit the reasoning behind the inversion tactic
+   and corresponds to reading the two above rules from the bottom
+   up. It can be proven directly via inversion w/o any auxiliary
+   lemmas.  *)
+ 
+ 
+ End RepeatDefined.
+ 
+
+
