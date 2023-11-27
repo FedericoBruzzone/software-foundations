868179a54e13ba20e6fb3a6e513ee0ac378f0d6b
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-11-27 19:26:20 +0100

    Update
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- assignements/7/w.v
+++ assignements/7/w.v
@@ -18,10 +18,34 @@ From PLF Require Import Equiv.
 
 Theorem swap_if_branches : forall b c1 c2,
     cequiv
-         <{ if b then c1 else c2 end }>
+    <{ if b then c1 else c2 end }>
     <{ if ~ b then c2 else c1 end }>.
 Proof.
-Admitted.
+  intros.
+  split; intros H.
+  - inversion H; subst.
+    + apply E_IfFalse.
+      -- simpl.
+         rewrite H5.
+         reflexivity.
+      -- assumption.
+    + apply E_IfTrue.
+      -- simpl.
+         rewrite H5.
+         reflexivity.
+      -- assumption.
+  - inversion H; subst.
+    + apply E_IfFalse.
+      -- simpl in H5. (* Search negb. *)
+         rewrite negb_true_iff in H5.
+         assumption.
+      -- assumption.
+    + apply E_IfTrue.
+      -- simpl in H5. (* Search negb. *)
+         rewrite negb_false_iff in H5.
+         assumption.
+      -- assumption.
+Qed.
 
 (** 2. Now, go back to Imp.v and the following definition*)
 
@@ -38,10 +62,24 @@ Proof.
   inversion Heval. subst. clear Heval. simpl.
   apply Maps.t_update_eq.  Qed.
 
-
 (** What you have to do is state and prove a very similar result for
 the program [subtract_slowly_body] *)
 
 Print subtract_slowly_body.
 
+Theorem subtract_slowly_body_spec : forall st st' n m,
+  st X = n ->
+  st Z = m ->
+  st =[ subtract_slowly_body ]=> st' ->
+  st' X = n - 1 /\ st' Z = m - 1.
+Proof.
+  intros st st' n m HX HZ H.
+  inversion H; subst.
+  inversion H2; subst.
+  inversion H5; subst.
+  simpl in *.
+  split.
+  - apply Maps.t_update_eq.
+  - apply Maps.t_update_eq with (m:=st)(x:=Z).
+Qed.
 
