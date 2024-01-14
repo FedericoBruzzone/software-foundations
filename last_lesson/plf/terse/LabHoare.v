7caf8eb44e55532062ec535cf8b1068d6b8525be
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2024-01-14 15:10:59 +0100

    Update LabHoare
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- last_lesson/plf/terse/LabHoare.v
+++ last_lesson/plf/terse/LabHoare.v
@@ -5,7 +5,7 @@ From Coq Require Import Arith.Arith.
 From Coq Require Import Arith.EqNat.
 From Coq Require Import Arith.PeanoNat. Import Nat.
 From Coq Require Import Lia.
-From PLF Require Export   Imp.
+From PLF Require Export Imp.
 From PLF Require Export Hoare.
 From PLF Require Export Vcg.
 Ltac verify_assn :=
@@ -41,9 +41,33 @@ Ltac verify_assn :=
 
 (** A. Prove that the following structural rule is derivable:*)
 Theorem spec_conj: forall P1 P2 Q1 Q2 C,
-{{P1}}  C {{Q1}} ->
-{{P2}}  C {{Q2}} -> {{P1 /\ P2}}  C {{Q1 /\ Q2}}.
-Admitted.
+{{P1}} C {{Q1}} ->
+{{P2}} C {{Q2}} -> {{P1 /\ P2}}  C {{Q1 /\ Q2}}.
+Proof.
+  intros.
+  unfold valid_hoare_triple.
+  intros.
+  destruct H2.
+  split.
+  - unfold valid_hoare_triple in H.
+    remember (H st st').
+    apply q.
+    + trivial.
+    + trivial.
+  - unfold valid_hoare_triple in H0.
+    remember (H0 st st').
+    apply q.
+    + trivial.
+    + trivial.
+Restart.
+  intros.
+  unfold valid_hoare_triple.
+  intros.
+  destruct H2.
+  split.
+  - verify_assn.
+  - verify_assn.
+Qed.
 
 (** B. Write a program that stores in a variable the max
 of two vars and prove it correct. You can use the library
@@ -52,27 +76,72 @@ with an explicit [fun st => ...] or you can use the below lifted version. *)
 Definition MAX :=
   ap2 max X Y.
 
+Print ap2.
+
 (** Replace the bogus spec and skip with your own code*)
 Lemma max_correct:
-{{True}}
-<{ skip}>
-{{ True }} .
-Admitted.
+{{ True }}
+<{
+  if X <= Y then
+    Z := Y
+  else
+    Z := X
+  end
+}>
+{{ X > Y /\ Z = X \/ X <= Y /\ Z = Y }}.
+Proof.
+  apply hoare_if; try (apply hoare_asg_conseq; verify_assn).
+Qed.
+
 
 (** C.  The following program computes the substraction [p - n].
 Show that the triple is valid by _finding_ the right invariant.
 We suggest you first do this on paper.*)
 
-Example subtract_slowly_correct (m p :  nat)  :
+Example subtract_slowly_correct (m p :  nat)  :=
   {{ X = m /\  Z = p }}
   <{ while (X <> 0)  do
      Z := Z - 1 ;
      X := X - 1
      end }>
     {{ Z = p - m }} .
-Admitted.
+
+Definition inv_subtract_slowly_correct (m p : nat) : Assertion :=
+  fun st => (st Z) = p - m + (st X).
 
 (** D. Rewrite the program as an annotated program [acom]. Use the
 [vc] verification function to generate the verification condition and re-prove it.  Hint: print
 the previous program as a [com] _without_ notations and use it as a skeleton *)
+
+Print subtract_slowly_correct.
+Locate BNot.
+
+Example acom_subtract_slowly_correct (m p : nat) :=
+  CWhile (inv_subtract_slowly_correct m p)
+         (BNot (BEq (AId X) (ANum 0)))
+         (CSeq
+           (CAsgn Z (AMinus (AId Z) 1))
+           (CAsgn X (AMinus (AId X) 1))).
+
+Theorem proof_subtract_slowly_correct:
+  forall (m p : nat), vc (acom_subtract_slowly_correct m p) (fun st => (st Z) = p - m).
+Proof.
+  unfold acom_subtract_slowly_correct.
+  unfold inv_subtract_slowly_correct.
+  unfold vc.
+  intros.
+  split.
+  - intros.
+    split.
+    + intros.
+      inversion H; subst.
+      inversion H1; subst.
+Restart.
+  unfold acom_subtract_slowly_correct.
+  unfold inv_subtract_slowly_correct.
+  verify_assn.
+  Show Proof.
+Qed.
+
+
 
